e36a52ceaa50f5bbb37d95a7203b65779249de93
Einstein <przemekworld@gmail.com>
2014-12-08 18:16:58 +0100

    Inicjalizacja projektu

---
+++ LiczbyNaSlowaNET_Testy/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LiczbyNaSlowaNET;
+
+namespace LiczbyNaSlowaNET_Testy
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           
+
+        }
+    }
+}
