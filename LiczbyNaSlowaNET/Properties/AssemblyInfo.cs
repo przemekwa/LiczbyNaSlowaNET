b0d8f6aed790f94f50824017c0b8adbf3694ee1a
Einstein <przemekworld@gmail.com>
2014-12-08 21:55:22 +0100

    Zmiana TargetFramework na 4.0

--- LiczbyNaSlowaNET/Properties/AssemblyInfo.cs
+++ LiczbyNaSlowaNET/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LiczbyNaSlowaNET")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
