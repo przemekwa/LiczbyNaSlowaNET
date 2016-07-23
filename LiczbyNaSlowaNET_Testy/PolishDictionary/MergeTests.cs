75aecd193bc07bffa8d2749e7c6457a2685439af
Przemek <przemekworld@gmail.com>
2016-07-23 21:55:24 +0200

    Nowa propozycja wywoływania metody NumberToText

---
+++ LiczbyNaSlowaNET_Testy/PolishDictionary/MergeTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LiczbyNaSlowaNET;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LiczbyNaSlowaNET_Testy.PolishDictionary
+{
+    [TestClass]
+    public class MergeTests
+    {
+        [TestMethod]
+        public void NewWay()
+        {
+            // TODO: Pomysł 1
+
+            var test = NumberToText.Convert(2.3M, new NumberToTextOptions
+            {
+                Dictionary = new PolishWithsStemsDictionary();
+            });
+
+        //TODO: Pomysł 2
+
+            var test = NumberToText.Convert(2.3M, new NumberToTextOptions
+            {
+                Converter = Converter.PL
+            })
+
+
+             var test = NumberToText.Convert(2.3M, new NumberToTextOptions
+             {
+                 Converter = Converter.PL2 //With stems
+             })
+
+
+            var test = NumberToText.Convert(2.3M, new NumberToTextOptions
+            {
+                Converter = Converter.PL
+                CurrencySymbol = Currency.PLN,
+            })
+
+
+             var test = NumberToText.Convert(2.3M, new NumberToTextOptions
+             {
+                 Converter = Converter.PL2 //With stems
+                 CurrencySymbol = Currency.PLN,
+                 SplitDecimal = ".",
+             })
+
+
+        }
+    }
+}
